7901d819b7f4aecf7a6da650ab4ae1bbddd9297d
amalcheriyan <amalcheriyan.007@gmail.com>
2024-06-20 19:10:06 +0400

    cartnewpage

--- lib/screens/Cart/cart_screen.dart
+++ lib/screens/Cart/cart_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/Provider/cart_provider.dart';
+import 'package:shopping_app/screens/nav_bar_screen.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -10,25 +12,128 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
+    final provider = CartProvider.of(context);
+    final finalList = provider.cart;
     return Scaffold(
         body: SafeArea(
       child: Column(
         children: [
           Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(children: [
-              IconButton(
-                style: IconButton.styleFrom(
-                  padding: const EdgeInsets.all(20),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                iconSize: 30,
-                icon: const Icon(Icons.arrow_back),
-              ),
-            ]),
-          )
+            padding: const EdgeInsets.all(8),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: const EdgeInsets.all(20),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const BottomNavBar(),
+                        ),
+                      );
+                    },
+                    iconSize: 25,
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                  const Text(
+                    "My Cart",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Container(),
+                ]),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: finalList.length,
+              itemBuilder: (context, index) {
+                final cartItems = finalList[index];
+                return Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 243, 237, 237),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 80,
+                              width: 80,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: Image.asset(cartItems.image),
+                            ),
+                            const SizedBox(width: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  cartItems.title,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  cartItems.material,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                    color: Colors.grey.shade400,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "\$${cartItems.price}",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                    color: Color.fromARGB(255, 238, 125, 26),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                        top: 35,
+                        right: 35,
+                        child: Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                finalList.removeAt(index);
+                                setState(() {});
+                              },
+                              icon: const Icon(
+                                Icons.delete,
+                                color: Color.fromARGB(255, 175, 30, 20),
+                                size: 25,
+                              ),
+                            ),
+                          ],
+                        ))
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
     ));
